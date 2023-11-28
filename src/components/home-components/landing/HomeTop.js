edf8d500f5be5fd6bb3af960ecf6fe5d867c45b6
LeanM <42581931+LeanM@users.noreply.github.com>
2023-11-28 16:12:22 -0300

    Responsive fixes

---
+++ src/components/home-components/landing/HomeTop.js
@@ -0,0 +1,57 @@
+import "../../../css/home.css";
+import { colors } from "../../../assets/colors";
+import asado from "../../../assets/ASADO.jpg";
+import { createUseStyles } from "react-jss";
+
+export default function HomeTop() {
+  const classes = useStyles();
+
+  return (
+    <div className="home__top">
+      <img className={classes.image} src={asado}></img>
+      <div className={classes.dataContainer}>
+        <p
+          style={{
+            fontWeight: "800",
+            color: colors.nav,
+            textShadow: "black 0 0 10px",
+          }}
+        >
+          Bienvenidos a Apetito Granada!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+const useStyles = createUseStyles({
+  image: {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    filter: "brightness(80%)",
+  },
+  dataContainer: {
+    color: "white",
+    width: "50%",
+    height: "20rem",
+    fontSize: "3rem",
+    zIndex: 20,
+    position: "absolute",
+    textAlign: "center",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    top: "15%",
+    borderTop: `solid 20px ${colors.nav}`,
+    backgroundColor: colors.navSemiTransparent,
+
+    "@media screen and (max-width: 1000px)": {
+      width: "70%",
+    },
+
+    "@media screen and (max-width: 700px)": {
+      fontSize: "2rem",
+    },
+  },
+});
