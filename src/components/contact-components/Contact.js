a5172c0495182e786ffe178678f08d89557d1309
LeanM <42581931+LeanM@users.noreply.github.com>
2023-12-09 22:40:53 -0300

    Advance in contact

--- src/components/contact-components/Contact.js
+++ src/components/contact-components/Contact.js
@@ -3,159 +3,114 @@ import React, { useEffect, useRef, useState } from "react";
 import { Form, ButtonToolbar, Button, Input } from "rsuite";
 import { Steps } from "rsuite";
 import AlterNav from "../pagewrappers/AlterNav";
+import FormInput from "./FormInput";
 
 export default function Contact() {
+  const [step, setStep] = useState(0);
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    date: "",
+    time: "",
+    description: "",
+  });
   const classes = useStyles();
-  const [step, setStep] = useState(1);
-  const [errMsg, setErrMsg] = useState("");
+  const inputs = [
+    {
+      id: 1,
+      name: "name",
+      type: "text",
+      placeholder: "Your name",
+      errorMessage:
+        "Your name should be 3-16 characters and shouldn't include any special character!",
+      label: "Name",
+      pattern: "/^[a-z ,.'-]+$/i",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "date",
+      type: "date",
+      placeholder: "Select Event Date",
+      label: "Event Date",
+      required: true,
+    },
 
-  const userRef = useRef();
-  const errorRef = useRef();
+    {
+      id: 4,
+      name: "time",
+      type: "time",
+      placeholder: "Select Event Time",
+      label: "Event Time",
+    },
+    {
+      id: 5,
+      name: "description",
+      type: "text",
+      placeholder: "Anything you want to say...",
+      label: "Description",
+    },
+  ];
 
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [eventDate, setEventDate] = useState("");
-  const [eventTime, setEventTime] = useState("");
-  const [people, setPeople] = useState(0);
-  const [extraInfo, setExtraInfo] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
 
-  const Textarea = React.forwardRef((props, ref) => (
-    <Input {...props} as="textarea" ref={ref} />
-  ));
-  const formRef = useRef(null);
-
-  const styles = {
-    width: "200px",
-    display: "inline-table",
-    verticalAlign: "top",
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   useEffect(() => {
-    console.log(name);
-  }, [name]);
+    verifyStep();
+  }, [values]);
+
+  const verifyStep = () => {
+    let step = 0;
+    if (values.name !== "") step += 1;
+    if (values.email !== "") step += 1;
+    if (values.date !== "") step += 1;
+    if (values.time !== "") step += 1;
+
+    setStep(step);
+  };
 
   return (
     <>
       <AlterNav />
       <div className={classes.container}>
         <div className={classes.dataContainer}>
-          <form className={classes.form}>
-            <label className="login-register__label" htmlFor="name">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-              required
-            />
-            <label className="login-register__label" htmlFor="email">
-              Email
-            </label>
-            <input
-              type="text"
-              id="email"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              required
-            />
-            <label className="login-register__label" htmlFor="date">
-              Date
-            </label>
-            <input
-              type="date"
-              id="date"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setEventDate(e.target.value)}
-              value={eventDate}
-              required
-            />
-            <label className="login-register__label" htmlFor="time">
-              Time
-            </label>
-            <input
-              type="time"
-              id="time"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setEventTime(e.target.value)}
-              value={eventTime}
-              required
-            />
-            <label className="login-register__label" htmlFor="people">
-              People
-            </label>
-            <input
-              type="number"
-              id="people"
-              ref={userRef}
-              autoComplete="off"
-              onChange={(e) => setPeople(e.target.value)}
-              value={people}
-              required
-            />
+          <form onSubmit={handleSubmit}>
+            <h1>Contact us!</h1>
+            {inputs.map((input) => (
+              <FormInput
+                key={input.id}
+                {...input}
+                value={values[input.name]}
+                onChange={onChange}
+              />
+            ))}
+            <button>Submit</button>
           </form>
-          <Form
-            ref={formRef}
-            className={classes.form}
-            onCheck={() => setStep(2)}
-          >
-            <Form.Group controlId="name">
-              <Form.ControlLabel>Your Name</Form.ControlLabel>
-              <Form.Control name="name" />
-            </Form.Group>
-            <Form.Group controlId="email">
-              <Form.ControlLabel>Email</Form.ControlLabel>
-              <Form.Control name="email" type="email" />
-              <Form.HelpText tooltip>Email is required</Form.HelpText>
-            </Form.Group>
-            <Form.Group controlId="password">
-              <Form.ControlLabel>Event Date</Form.ControlLabel>
-              <Form.Control name="date" type="date" autoComplete="off" />
-            </Form.Group>
-            <Form.Group controlId="password">
-              <Form.ControlLabel>Event Time</Form.ControlLabel>
-              <Form.Control name="time" type="time" autoComplete="on" />
-            </Form.Group>
-            <Form.Group controlId="password">
-              <Form.ControlLabel>Event Number of People</Form.ControlLabel>
-              <Form.Control name="people" type="number" autoComplete="on" />
-            </Form.Group>
-            <Form.Group controlId="textarea">
-              <Form.ControlLabel>Extra info</Form.ControlLabel>
-              <Form.Control
-                rows={5}
-                name="textarea"
-                accepter={Textarea}
-                placeholder="Talk about something you want to know"
-              />
-            </Form.Group>
-            <Form.Group>
-              <ButtonToolbar>
-                <Button appearance="primary">Submit</Button>
-                <Button appearance="default">Cancel</Button>
-              </ButtonToolbar>
-            </Form.Group>
-          </Form>
-          <div>
-            <Steps current={step} vertical style={styles}>
-              <Steps.Item
-                title="Choose desired Menues"
-                description="Choosed wanted menus"
-              />
-              <Steps.Item
-                title="Complete your data"
-                description="Description"
-              />
-              <Steps.Item title="Submit your book" description="Description" />
-            </Steps>
-          </div>
+          <Steps current={step} vertical>
+            <Steps.Item
+              title="Choose desired Menues"
+              description="Choosed wanted menus"
+            />
+            <Steps.Item title="Complete your data" description="Description" />
+            <Steps.Item title="Submit your book" description="Description" />
+            <Steps.Item title="Submit your book" description="Description" />
+            <Steps.Item title="Submit your book" description="Description" />
+          </Steps>
         </div>
       </div>
     </>
