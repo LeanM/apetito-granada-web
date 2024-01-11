d8f97b2384c3f04c8eb33a4e444b2a215e15c102
Leandro Moran <42581931+LeanM@users.noreply.github.com>
2024-01-11 00:39:42 -0300

    new modal

--- src/components/home-components/categoryCards/MenuModal.js
+++ src/components/home-components/categoryCards/MenuModal.js
@@ -1,6 +1,9 @@
-import { Modal, Button, ButtonToolbar, Placeholder } from "rsuite";
 import { createUseStyles } from "react-jss";
 import { colors } from "../../../assets/colors";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 export default function MenuModal(props) {
   const { open } = props;
@@ -8,27 +11,18 @@ export default function MenuModal(props) {
 
   return (
     <Modal
-      overflow={false}
-      className={classes.container}
       open={open}
       onClose={props.onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
     >
-      <div className={classes.header}>
-        <p style={{ fontWeight: "700", fontSize: "1.2rem", color: colors.nav }}>
-          Menu Plates
-        </p>
-        <button className={classes.upperCloseButton} onClick={props.onClose}>
-          X
-        </button>
-      </div>
-      <Modal.Body>
-        <Placeholder.Paragraph rows={35} />
-      </Modal.Body>
-
-      <div className={classes.header}>
-        <button className={classes.closeButton} onClick={props.onClose}>
-          Close
-        </button>
+      <div className={classes.container}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Text in a modal
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+        </Typography>
       </div>
     </Modal>
   );
@@ -36,8 +30,20 @@ export default function MenuModal(props) {
 
 const useStyles = createUseStyles({
   container: {
-    width: "70%",
-    height: "80vh",
+    position: "absolute",
+    width: "75%",
+    height: "95%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: colors.nav,
+    border: "2px solid #000",
+    boxShadow: "0 0 5px black",
+    overflowY: "scroll",
     fontFamily: "Poppins",
   },
   header: {
